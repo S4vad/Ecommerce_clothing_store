1b80af3a5b94183d5e6adb9aaefb8a32a04397f8
s4vad <savad31@outlook.com>
2024-03-31 17:44:42 +0530

    uer login

--- main.js
+++ main.js
@@ -44,7 +44,7 @@ app.use('/user',userRoute);
 app.use(cookieParser());
 
 
-const port = process.env.PORT || 3005;
+const port = process.env.PORT || 3000;
 app.listen(port,()=>{
     console.log('server running successfully',port)
 })
