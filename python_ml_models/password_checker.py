466f6f42a87bd044c358703f1ea5f16d9818d052
savad k <124030718+S4vad@users.noreply.github.com>
2024-10-26 21:30:51 +0530

    passwordcheckermodel

--- python_ml_models/password_checker.py
+++ python_ml_models/password_checker.py
@@ -1,9 +1,7 @@
-
-
+import gzip
 import pickle
 import pandas as pd
 import numpy as np
-from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
@@ -11,58 +9,44 @@ from sklearn.ensemble import RandomForestClassifier
 data = pd.read_csv("../pass.csv", on_bad_lines='skip', encoding='latin-1')
 print(data.head())
 
-
-
-
 data = data.dropna()
-data["strength"] = data["strength"].map({0: "Weak", 
-                                         1: "Medium",
-                                         2: "Strong"})
+data["strength"] = data["strength"].map({0: "Weak", 1: "Medium", 2: "Strong"})
 print(data.sample(5))
 
-
-
-
-data=data[0:30000]
-
-
-
+data = data[0:50000]
 
 
 def word(password):
-    character=[]
-    for i in password:
-        character.append(i)
-    return character
-  
+    return list(password)
+
+
 x = np.array(data["password"])
 y = np.array(data["strength"])
 
+
 tdif = TfidfVectorizer(tokenizer=word)
 x = tdif.fit_transform(x)
-xtrain, xtest, ytrain, ytest = train_test_split(x, y, 
-                                                test_size=0.05, 
-                                                random_state=42)
 
 
-
+xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size=0.05, random_state=42)
 
 
 model = RandomForestClassifier()
 model.fit(xtrain, ytrain)
-print(model.score(xtest, ytest))
+print("Model Score:", model.score(xtest, ytest))
 
 
-with open("./python_ml_models/password_strength_model.pkl", "wb") as file:
-    pickle.dump(model, file)
+model_filename = 'python_ml_models/password_strength_model.pkl.gz'
+with gzip.open(model_filename, 'wb') as f:
+    pickle.dump(model, f)
+
+
+with gzip.open(model_filename, 'rb') as f:
+    model = pickle.load(f)
 
 
 import getpass
 user = getpass.getpass("Enter Password: ")
 data = tdif.transform([user]).toarray()
 output = model.predict(data)
-print(output)
-
-
-
-
+print("Password Strength:", output[0])
